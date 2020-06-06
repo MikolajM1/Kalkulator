466bcff265e92ea373a90c7843c712cb9d927df2
Mikołaj <mikififi12@gmail.com>
2020-06-06 10:49:29 +0200

    Dodanie getter

--- app/src/main/java/com/example/kalkulator/Data.java
+++ app/src/main/java/com/example/kalkulator/Data.java
@@ -3,4 +3,28 @@ package com.example.kalkulator;
 public class Data {
     String a;
     boolean b;
+    float f;
+    double d;
+
+    public Data(String a, boolean b, float f) {
+        this.a = a;
+        this.b = b;
+        this.f = f;
+    }
+
+    public String getA() {
+        return a;
+    }
+
+    public boolean isB() {
+        return b;
+    }
+
+    public float getF() {
+        return f;
+    }
+
+    public double getD() {
+        return d;
+    }
 }
